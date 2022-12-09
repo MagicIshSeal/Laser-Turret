ef80ca8ab2f09d010ecec0e3ab9bed10a60e370c
MagicIshSeal <maxvaneikeren@gmail.com>
2022-12-09 10:23:30 +0100

    cleaned up and entered new code

--- src/main.cpp
+++ src/main.cpp
@@ -7,51 +7,70 @@ Servo servo_y;
 // Pin definitions
 #define echoPin 3
 #define trigPin 2
+#define xpin_out 2
+#define ypin_out 3
+#define ypin_in A1
+#define xpin_in A0
+#define swPin 11
+#define lsPin 12
 
-long duration;
-int distance;
 const int rs = 10, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-
-//const int xpin = 2;
-//const int ypin = 3;
+long duration;
+int dist;
 
 void setup() {
-  lcd.begin(16, 2);
+  // Set pins to correct OUT and IN
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(xpin_in, INPUT);
+  pinMode(ypin_in, INPUT);
+  pinMode(swPin, INPUT);
+  pinMode(lsPin, OUTPUT);
+  //Attach servo to pin
+  servo_x.attach(xpin_out);
+  servo_y.attach(ypin_out);
+  // start Serial Monitor
   Serial.begin(9600);
-  // Define Servo X variables
-  pinMode(A0, INPUT);
-  //servo_x.attach(xpin);
-  // Define Servo y variables
-  pinMode(A1, INPUT);
-  //servo_y.attach(ypin);
+  //start LCD
+  lcd.begin(16, 2);
+  // Create a function that measures and writes the distance to the screen
+  int distance() {
+    // Makes sure the trigPin is LOW(inactive) before continuing
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    dis = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    // Displays the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.print(dist);
+    Serial.println(" cm");
+    // Displays the distance on the display
+    lcd.setCursor(0,0);
+    lcd.print(dist);
+    lcd.setCursor(4,0);
+    lcd.print("cm");
+    delay(200);
+    lcd.clear();
+    }
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  // call the function defined in the setup
+  distance();
   // Write the output of the joystick to the servos
-  //servo_x.write(analogRead(A0));
-  //servo_y.write(analogRead(A1));
-  lcd.setCursor(0,0);
-  lcd.print(distance);
-  lcd.setCursor(4,0);
-  lcd.print("cm");
-  delay(200);
-  lcd.clear();
+  servo_x.write(analogRead(xpin_in));
+  servo_y.write(analogRead(ypin_in));
+  // Check if the button is pressed and fire the laser
+  if (swPin = LOW){
+    digitalWrite(lsPin, HIGH);
+  }
+  
+
 }
